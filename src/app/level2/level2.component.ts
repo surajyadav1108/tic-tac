fbfbfd08304627132d2d436acebac6a334c9bac3
suraj yadav <harrypotter1108@gmail.com>
2022-10-14 13:03:53 +0530

    Add files via upload

---
+++ src/app/level2/level2.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-level2',
+  templateUrl: './level2.component.html',
+  styleUrls: ['./level2.component.css']
+})
+export class Level2Component {
+  private currentPlayerIx: number;
+  private currentWinnerIx: number;
+  private playerName: string[];
+
+  public boardContent: number[][];
+
+  constructor() {
+    this.playerName = ['', 'X', 'O',];
+    this.boardContent = [
+      [ 0, 0, 0],
+      [ 0, 0, 0],
+      [ 0, 0, 0],
+    ];
+    this.currentPlayerIx = 1; 
+    this.currentWinnerIx = 0;
+  }
+
+  public getPlayerName(col:number, row: number): string {
+    return this.playerName[this.boardContent[row][col]];
+  }
+
+
+  public getStyle(col: number, row: number): string {   
+    if (this.boardContent[row][col] !== 0 ) {
+      return `occupied-${this.getPlayerName(col, row)}`;
+    }
+    return '';
+  }
+ 
+
+
+  public set(col: number, row: number): void {
+    if (this.currentPlayerIx === 0 || this.boardContent[row][col] === 0){
+      this.boardContent[row][col] = this.currentPlayerIx;
+      this.currentPlayerIx = this.currentPlayerIx === 1 ? 2 : 1;
+    }
+  }
+}
