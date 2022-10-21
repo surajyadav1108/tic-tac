d710427efecc642af3f7154aaff969011919ab66
suraj yadav <harrypotter1108@gmail.com>
2022-10-21 10:02:09 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Level1Component } from './level1/level1.component';
+import { Level2Component } from './level2/level2.component';
+import { Level3Component } from './level3/level3.component';
 
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'level1'},
-  {path: 'level1', component: Level1Component }
+  {path: 'level1', component: Level1Component },
+  {path: 'level2', component: Level2Component },
+  {path: 'level3', component: Level3Component },
 ];
 
 @NgModule({
