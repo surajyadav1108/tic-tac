2da062cb266be2b07b26246e02f4381777bcd66c
suraj yadav <harrypotter1108@gmail.com>
2022-10-21 08:58:17 +0530

    Update level2.component.ts

--- src/app/level2/level2.component.ts
+++ src/app/level2/level2.component.ts
@@ -36,6 +36,12 @@ export class Level2Component {
       this.boardContent[row][col] = this.currentPlayerIx;
       this.currentPlayerIx = this.currentPlayerIx === 1 ? 2 : 1;
     }
+    this.currentWinnerIx = this.getWinnerIndex();
+  }
+  
+  
+  public get winnerIndex(): number {
+    return this.currentWinnerIx;
   }
   
    public onRestart(): void {
@@ -47,5 +53,57 @@ export class Level2Component {
     this.currentPlayerIx = 1; 
     this.currentWinnerIx = 0;
   }
+  
+  /*Return the winner 
+  */
+
+  private getWinnerIndex(): number  {
+    // check for rows
+    for (let row = 0; row < 3; row++) {
+      const first = this.boardContent[row][0];
+      if (
+        first !== 0 &&
+        this.boardContent[row][1] === first &&
+        this.boardContent[row][2] === first
+      ) {
+        return first;
+      }
+    }
+
+    //check columns
+
+    for ( let col =0; col < 3; col++) {
+      const first = this.boardContent[0][col];
+      if (
+        first !==0 && 
+        this.boardContent[1][col] === first &&
+        this.boardContent[2][col] === first
+      ) {
+        return first;
+      }
+    }
+
+    //chech diagonals
+
+    const first = this.boardContent[1][1];
+    if ( first !== 0) {
+      if  (
+        this.boardContent[0][0] === first &&
+        this.boardContent[2][2] === first
+      ) {
+        return first;
+      }
+      if (
+        this.boardContent[2][0] === first &&
+        this.boardContent[0][2] === first
+      ) {
+        return first;
+      }
+    }
+
+    return 0;
+  }
 
 }
+
+
