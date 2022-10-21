f9c561a6f429a72be55a039f8c69279ff08e656d
suraj yadav <harrypotter1108@gmail.com>
2022-10-21 10:03:25 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Level1Component } from './level1/level1.component';
+import { Level2Component } from './level2/level2.component';
+import { Level3Component } from './level3/level3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Level1Component,
+    Level2Component,
+    Level3Component,
   ],
   imports: [
     BrowserModule,
