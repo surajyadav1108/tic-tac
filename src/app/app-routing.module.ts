31dc33298f3dc8f4543ad567edd24bd1ada78f12
suraj yadav <harrypotter1108@gmail.com>
2022-10-26 20:00:32 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { Level1Component } from './level1/level1.component';
 import { Level2Component } from './level2/level2.component';
 import { Level3Component } from './level3/level3.component';
 import { Level4Component } from './level4/level4.component';
+import { Level5Component } from './level5/level5.component';
+
 
 
 const routes: Routes = [
@@ -12,6 +14,7 @@ const routes: Routes = [
   {path: 'level2', component: Level2Component },
   {path: 'level3', component: Level3Component },
   {path: 'level4', component: Level4Component },
+  {path: 'level5', component: Level5Component },
 ];
 
 @NgModule({
