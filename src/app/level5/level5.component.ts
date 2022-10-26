5f3ad0e2c6c7ac85888224fb1c9791d623095077
suraj yadav <harrypotter1108@gmail.com>
2022-10-26 20:15:44 +0530

    Add files via upload

---
+++ src/app/level5/level5.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-level5',
+  templateUrl: './level5.component.html',
+  styleUrls: ['./level5.component.css']
+})
+export class Level5Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
